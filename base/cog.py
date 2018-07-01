900b91503305ba592e535c657fbeb134f37d333f
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-07-01 13:18:11 +0300

    Added command with the invite to the development server.

--- base/cog.py
+++ base/cog.py
@@ -13,3 +13,10 @@ class Cog(CogBase):
     @test.command()
     async def ping(self, ctx: Context) -> None:
         await ctx.post(Message('pong'))
+
+    @commands.command()
+    async def contact(self, ctx: Context) -> None:
+        """
+        Shows an invite to the bot development server.
+        """
+        await ctx.post(Message('https://discord.gg/gE9Dh57'))
