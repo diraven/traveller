ca3aec3abae1ed3152d339dec0ea00979c2cf9a0
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-09-02 18:00:50 +0300

    Another attempt to fix incorrect offset testing and message with "more" not showing up. Hopefully, last.

--- publicroles/cog.py
+++ publicroles/cog.py
@@ -55,7 +55,7 @@ def format_list(items: List[Role], offset: int = 0) -> Tuple[str, int]:
             names.append(item.name)
             offset += 1
         result = ", ".join(['`{}`'.format(name) for name in names])
-        if offset + len(names) < len(items):
+        if offset < len(items):
             result += "... Click button below to see {} more.".format(
                 len(items) - len(names) - (offset - 1)
             )
