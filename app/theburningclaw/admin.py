5e62a7d68089ee8b67573f01decebe90982674ae
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2019-05-25 19:18:56 +0300

    Add simple ventures system.

--- app/theburningclaw/admin.py
+++ app/theburningclaw/admin.py
@@ -1,7 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
-from theburningclaw.models import Character, ThingKind
+from theburningclaw.models import Character, ThingKind, Venture, Thing, \
+    Lootable
+
+
+class ThingInlineAdmin(admin.TabularInline):
+    model = Thing
+    extra = 1
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    inlines = (ThingInlineAdmin,)
+
+
+admin.site.register(Character, CharacterAdmin)
 
 admin.site.register(ThingKind)
-admin.site.register(Character)
+
+
+class LootableInlineAdmin(admin.TabularInline):
+    model = Lootable
+    extra = 1
+
+
+class VentureAdmin(admin.ModelAdmin):
+    inlines = (LootableInlineAdmin,)
+
+
+admin.site.register(Venture, VentureAdmin)
--- app/theburningclaw/views.py
+++ app/theburningclaw/views.py
@@ -1,10 +1,13 @@
 # Create your views here.
+import random
 from collections import namedtuple
 
 from allauth.socialaccount.models import SocialAccount, SocialApp
 from rest_framework.exceptions import ValidationError, AuthenticationFailed
-from rest_framework.response import Response
+from rest_framework.response import Response as DRFResponse
 from rest_framework.views import APIView
+
+from theburningclaw.models import Venture
 
 Request = namedtuple('Request', [
     'service_name',
@@ -13,6 +16,23 @@ Request = namedtuple('Request', [
     'display_name',
     'query',
 ])
+
+
+class Response:
+    def __init__(self):
+        self.character = None
+        self.events = []
+
+    def add_event(self, text: str = ''):
+        self.events.append(text)
+
+    def drf(self):
+        if not self.events:
+            self.events += ['Nothing happened.']
+        return DRFResponse({
+            'character': str(self.character),
+            'events': self.events,
+        })
 
 
 class Do(APIView):
@@ -34,17 +54,41 @@ class Do(APIView):
         except SocialAccount.DoesNotExist:
             raise AuthenticationFailed()
 
+        resp = Response()
+
         # Get user and character.
         try:
             character = SocialAccount.objects.get(
                 uid=req.service_user_id,
             ).user.character
         except SocialAccount.DoesNotExist:
-            return Response('needs registeration')
+            resp.add_event('Registration required.')
+            return resp.drf()
+        resp.character = character
 
         # Process request.
+        action, arg = req.query.split(' ')
+        if action == 'venture':
+            try:
+                venture = Venture.objects.get(slug=arg)
+            except Venture.DoesNotExist:
+                resp.add_event('Venture not found.')
+                return resp.drf()
 
-        return Response({
-            "message": "Hello, world!",
-            'character': str(character),
-        })
+            luck = random.uniform(0, 1)
+            for lootable in venture.lootable_set.filter(
+                    chance__gte=1 - luck,
+            ):
+                count = random.randint(
+                    lootable.count - lootable.count_deviation,
+                    lootable.count + lootable.count_deviation + 1,
+                )
+                character.give(
+                    (lootable.thing_kind, count),
+                )
+                resp.add_event(
+                    f'You have looted {count} x {lootable.thing_kind}'
+                )
+            return resp.drf()
+
+        return resp.drf()
