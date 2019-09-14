92f88f3f4134bc47ab8ab8a5118c3cb873f245e0
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2019-09-14 19:43:52 +0300

    Change publicroles command to publicrole.

--- app/extensions/publicroles/cog.py
+++ app/extensions/publicroles/cog.py
@@ -105,7 +105,7 @@ class Cog(CogBase):
     @commands.group(
         invoke_without_command=True,
     )
-    async def publicroles(
+    async def publicrole(
             self,
             ctx: Context,
             *args: str,
@@ -140,7 +140,7 @@ class Cog(CogBase):
             color=discord.Color.blue(),
         ).post()
 
-    @publicroles.command()
+    @publicrole.command()
     async def my(
             self,
             ctx: Context,
@@ -158,7 +158,7 @@ class Cog(CogBase):
             color=discord.Color.blue(),
         ).post()
 
-    @publicroles.command()
+    @publicrole.command()
     async def who(
             self,
             ctx: Context,
@@ -186,7 +186,7 @@ class Cog(CogBase):
                 color=discord.Color.blue(),
             ).post()
 
-    @publicroles.command()
+    @publicrole.command()
     async def join(
             self,
             ctx: Context,
@@ -198,7 +198,7 @@ class Cog(CogBase):
             await ctx.author.add_roles(role)
             await ctx.ok()
 
-    @publicroles.command()
+    @publicrole.command()
     async def leave(
             self,
             ctx: Context,
@@ -210,7 +210,7 @@ class Cog(CogBase):
             await ctx.author.remove_roles(role)
             await ctx.ok()
 
-    @publicroles.command()
+    @publicrole.command()
     async def top(
             self,
             ctx: Context,
