1f1d93eb625c5e56ad30db3939d7419e38bfbd4a
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2019-09-14 19:32:05 +0300

    Finish aliases management.

--- app/core/cog.py
+++ app/core/cog.py
@@ -1,7 +1,8 @@
 """Publicroles cog module."""
-
+import discord
 from discord.ext import commands
 
+from core import utils
 from core.cogbase import CogBase
 from core.context import Context
 from core.message import Message
@@ -11,9 +12,8 @@ from core.models import Guild
 class Cog(CogBase):
     """Core cog."""
 
-    @commands.group(
-        invoke_without_command=True,
-    )
+    @commands.group(invoke_without_command=True)
+    @commands.has_permissions(administrator=True)
     async def alias(
             self,
             ctx: Context,
@@ -21,47 +21,61 @@ class Cog(CogBase):
     ) -> None:
         """Show available aliases."""
         guild, _ = await Guild.get_or_create(ctx.guild.id)
-        print(guild.get('aliases', []))
+        await utils.Paginator(
+            ctx=ctx,
+            member=ctx.author,
+            items=[
+                f'`{v["src"]}` -> `{v["dst"]}`' for v in
+                guild.get('aliases', {})
+            ],
+            separator='\n',
+            title='command aliases',
+            color=discord.Color.blue(),
+        ).post()
 
-        # TODO: Format and output as paginator.
-        # guild = guilds.g
-        #
-        # guilds = ctx.bot.db.guilds
-        # print('result %s' % repr(result.inserted_id))
-        #
-        # await utils.Paginator(
-        #     ctx=ctx,
-        #     member=ctx.author,
-        #     items=['jhg'],
-        #     separator='\n',
-        #     timeout=60,
-        #     title='public roles found',
-        #     color=discord.Color.blue(),
-        # ).post()
-
-    @alias.command()
-    async def add(
+    @alias.command(name='add')
+    async def add_alias(
             self,
             ctx: Context,
             *args: str,
     ) -> None:
-        """Output your public roles."""
-        # TODO: Verify permissions.
+        """Add new alias."""
         if len(args) != 2:
             await ctx.post(Message.danger(
                 'Exactly 2 parameters must be provided for this command.',
             ))
             return
-        result = await Guild.add_alias(
+        count = await Guild.add_alias(
             guild_id=ctx.guild.id,
             src=args[0],
             dst=args[1],
         )
-        if result.modified_count:
+        if count:
             await ctx.ok()
         else:
             await ctx.post(Message.danger(
                 'Such alias already exists.',
             ))
 
-    # TODO: alias del command
+    @alias.command(name='del')
+    async def del_alias(
+            self,
+            ctx: Context,
+            *args: str,
+    ) -> None:
+        """Delete alias."""
+        if len(args) != 1:
+            await ctx.post(Message.danger(
+                'Exactly 1 parameters must be provided for this command.',
+            ))
+            return
+        count = await Guild.del_alias(
+            guild_id=ctx.guild.id,
+            src=args[0],
+        )
+        if count:
+            await ctx.ok()
+        else:
+            await ctx.post(Message.danger(
+                'No such alias was found.',
+            ))
