4f3228b0f3c4757ef638a22692c6d8829a8f1df2
TimOrdo <towkep@imbox.ru>
2019-10-22 18:52:59 +0300

    Rebuild of autorole module.

--- app/extensions/autorole/cog.py
+++ app/extensions/autorole/cog.py
@@ -17,76 +17,46 @@ class Cog(CogBase):
     async def autorole(self, ctx: Context) -> None:
         """Show autorole's status."""
         result = await Autorole.get(ctx.guild.id)
-        embed = discord.Embed(
-            title='Status of autorole function.',
-            colour=discord.Colour.blue(),
-        )
-        role = 'Not set yet.'
-        delay = 'Not set yet'
         if result:
+            delay = result['delay']
+            role = ctx.guild.get_role(result['role_id'])
             try:
-                delay = result['delay']
-            except KeyError:
-                delay = 'Not set yet'
-            for item in ctx.guild.roles:
-                if item.id == int(result['role']):
-                    role = item.mention
-                    break
-            status = 'Online'
+                await ctx.post_info(f'{role.mention} with {delay}m delay.')
+            except AttributeError:
+                await ctx.post_error('Role not found')
         else:
-            status = 'Offline'
-        embed.add_field(name='On/off status.', value=status, inline=False)
-        embed.add_field(name='Established role.', value=role, inline=False)
-        embed.add_field(
-            name='Time delay in minutes.',
-            value=str(delay),
-            inline=False,
-        )
-        await ctx.channel.send(embed=embed)
+            await ctx.post_info(f'Not set.')
 
     @autorole.command()
     @utils.is_owner_or_admin()
-    async def shutdown(self, ctx: Context):
+    async def disable(self, ctx: Context):
         """Shutdown of work autorole module."""
         await Autorole.guild_delete(ctx.guild.id)
-        await ctx.post_info('Autorole module is offline.')
+        await ctx.post_info('Autorole disabled.')
 
     @autorole.command()
     @utils.is_owner_or_admin()
-    async def setrole(self, ctx: Context, *, arg):
+    async def set(self, ctx: Context, role: discord.Role, delay: float):
         """Set role for autorole module."""
-        for item in ctx.guild.roles:
-            if arg in item.name:
-                await Autorole.update_role(ctx.guild.id, item.id)
-                await ctx.post_info(
-                    f'**Role has been changed**.\n'
-                    f'New role is {arg}.',
-                )
-                break
+        if role:
+            await Autorole.set(ctx.guild.id, role.id, float(delay))
+            await ctx.post_info(
+                f'Autorole was set to {role.mention} '
+                f'with {delay} minute(s) delay.',
+            )
         else:
             await ctx.post_error(
                 text='The role is set incorrectly.',
             )
-
-    @autorole.command()
-    @utils.is_owner_or_admin()
-    async def setdelay(self, ctx: Context, delay: float):
-        """Set delay for autorole module."""
-        await Autorole.get(ctx.guild.id)
-        await Autorole.set_delay(ctx.guild.id, float(delay))
-        await ctx.post_info(
-            f'**Delay has been changed.**\n '
-            f'New delay is {delay} minute(s).',
-        )
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         """Event that occurs after the user joins."""
         result = await Autorole.get(member.guild.id)
         if result:
-            for item in member.guild.roles:
-                if int(result['role']) == item.id:
-                    role = item
-                    await asyncio.sleep(float(result['delay']) * 60)
-                    await member.add_roles(role)
-                    break
+            role = member.guild.get_role(result['role_id'])
+            if role:
+                await asyncio.sleep(float(result['delay']) * 60)
+                await member.add_roles(role)
+            else:
+                await Autorole.guild_delete(member.guild.id)
