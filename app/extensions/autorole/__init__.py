cce1caa5d7aed0f1c3ff7e81f590fb2572d1a774
TimOrdo <towkep@imbox.ru>
2019-10-22 14:10:44 +0300

    Autorole module was added.
    Autorole module was added.

---
+++ app/extensions/autorole/__init__.py
@@ -0,0 +1,9 @@
+"""Autorole extension package."""
+from discord.ext import commands
+
+from .cog import Cog
+
+
+def setup(bot: commands.Bot) -> None:
+    """Set up mod with autorole functionality."""
+    bot.add_cog(Cog(bot))
