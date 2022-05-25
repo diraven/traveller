c86105e3f2c37540fc2f1f0a15bba512afcc995c
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2022-05-25 13:39:43 +0300

    feat: feat: disable role commands

--- commands.ts
+++ commands.ts
@@ -1,5 +1,4 @@
 import * as aftermath from './commands/aftermath/index';
 import * as faq from './commands/faq';
-import * as games from './commands/roles';
 
-export const commands = [games, faq, aftermath];
+export const commands = [faq, aftermath];
