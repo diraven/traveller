6da4fb865ecb47d0ff0ed3a5188c9dfaf7533c3f
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2022-08-10 18:39:04 +0300

    feat: support multiple servers

--- config.ts
+++ config.ts
@@ -5,3 +5,10 @@ export const DISCORD_APPLICATION_ID = process.env.DISCORD_APPLICATION_ID;
 export const DISCORD_PUBLIC_KEY = process.env.DISCORD_PUBLIC_KEY;
 export const DISCORD_BOT_GUILD_ID = process.env.DISCORD_BOT_GUILD_ID;
 export const DEBUG = process.env.DEBUG;
+
+export const DISCORD_GUILD_IDS = new Map([
+  ['diraven', '959896609787887706'],
+  ['ruthenia', '739848572526264450'],
+  ['the_ukrainians', '996037286267453440'],
+  ['ugc', '205691838760353792'],
+]);
--- deploy-commands.ts
+++ deploy-commands.ts
@@ -3,19 +3,21 @@ import { Routes } from 'discord-api-types/v10';
 import { commands } from './commands';
 import {
   DISCORD_APPLICATION_ID,
-  DISCORD_BOT_GUILD_ID,
   DISCORD_BOT_TOKEN,
+  DISCORD_GUILD_IDS,
 } from './config';
 
 const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN);
 
-rest
-  .put(
-    Routes.applicationGuildCommands(
-      DISCORD_APPLICATION_ID,
-      DISCORD_BOT_GUILD_ID,
-    ),
-    { body: commands.map((cmd) => cmd.command.toJSON()) },
-  )
-  .then(() => console.log('Successfully registered application commands.'))
-  .catch(console.error);
+DISCORD_GUILD_IDS.forEach((guild_id, guild_name) => {
+  rest
+    .put(Routes.applicationGuildCommands(DISCORD_APPLICATION_ID, guild_id), {
+      body: commands.map((cmd) => cmd.command.toJSON()),
+    })
+    .then(() =>
+      console.log(
+        `${guild_name}: successfully registered application commands.`,
+      ),
+    )
+    .catch(console.error);
+});
