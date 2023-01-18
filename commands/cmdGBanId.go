67ed9ae37fdea7fa77768ae2587649fcd142db18
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2023-01-18 21:07:59 +0200

    enrich gban command response

--- commands/cmdGBanId.go
+++ commands/cmdGBanId.go
@@ -50,23 +50,41 @@ var cmdGBanUid = &Command{
 			optionMap[opt.Name] = opt
 		}
 
-		// Collect ban errors if any.
-		errors := []string{}
+		// Collect ban messages if any.
+		messages := []string{}
 		for guildId := range state.State.Guilds {
-			err := s.GuildBanCreateWithReason(guildId, optionMap["user_id"].StringValue(), optionMap["reason"].StringValue(), 1)
+			guild, err := s.State.Guild(guildId)
 			if err != nil {
-				errors = append(errors, guildId+": "+err.Error())
+				messages = append(messages, "Не можу знайти сервер: "+guildId)
+			}
+			err = s.GuildBanCreateWithReason(guildId, optionMap["user_id"].StringValue(), optionMap["reason"].StringValue(), 1)
+			if err != nil {
+				messages = append(messages, "**"+guild.Name+"**: "+err.Error())
+			} else {
+				messages = append(messages, "**"+guild.Name+"**: ✅")
 			}
 		}
 
 		// Respond.
+		user, err := s.User(optionMap["user_id"].StringValue())
+		userName := ""
+		if err != nil {
+			userName = err.Error()
+		} else {
+			userName = user.Username + "#" + user.Discriminator + "(" + user.Mention() + ")"
+		}
 		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
 				Embeds: []*discordgo.MessageEmbed{
 					{
-						Title:       "Глобальний бан користувача по ідентифікатору: " + optionMap["user_id"].StringValue(),
-						Description: strings.Join(errors, "\n"),
+						Title:       "Глобальний бан",
+						Description: strings.Join(messages, "\n"),
+						Fields: []*discordgo.MessageEmbedField{
+							{Name: "Користувач", Value: userName},
+							{Name: "Ідентифікатор користувача", Value: optionMap["user_id"].StringValue()},
+							{Name: "Причина", Value: optionMap["reason"].StringValue()},
+						},
 					},
 				},
 			},
