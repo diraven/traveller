6dea2f0059ccdbcfb01005e5d8a62bf6de1250a1
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2023-07-08 23:00:28 +0300

    feat: command very important for ukraine

---
+++ cogs/rusni_pyzda.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+import aiohttp
+import discord
+from discord.ext import commands
+
+import settings
+
+API_ROOT_URL = "https://russianwarship.rip/api/v2"
+
+
+class RusniPyzdaCog(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self._bot = bot
+        super().__init__()
+
+    @discord.app_commands.command(description="Втрати РФ станом на сьогодні.")  # type: ignore
+    @discord.app_commands.guilds(discord.Object(id=settings.GUILD_ID))
+    @discord.app_commands.guild_only()
+    async def rusni_pyzda(self, interaction: discord.Interaction[commands.Bot]) -> None:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                f"{API_ROOT_URL}/statistics/{datetime.today().strftime('%Y-%m-%d')}"
+            ) as resp:
+                data = await resp.json()
+                if "errors" in data:
+                    embed = discord.Embed(
+                        title="Втрати ворога: помилка",
+                        description=data["message"],
+                        color=discord.Color.red(),
+                    )
+                else:
+                    data = data["data"]
+                    embed = discord.Embed(
+                        title="Втрати ворога",
+                        url=data["resource"],
+                    )
+                    for key, val in data["stats"].items():
+                        embed.add_field(name=key, value=val, inline=True)
+                    embed.set_footer(
+                        text=f"Станом на {data['date']}, {data['day']}й день війни"
+                    )
+
+                await interaction.response.send_message(
+                    embed=embed,
+                )
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import discord
 from discord.ext import commands
 
 import settings
-from cogs import faq
+from cogs import faq, rusni_pyzda
 
 bot = commands.Bot("!", intents=settings.intents)
 
 
 @bot.event
 async def on_ready() -> None:
-    await bot.add_cog(faq.Cog(bot))
+    await bot.add_cog(faq.FaqCog(bot))
+    await bot.add_cog(rusni_pyzda.RusniPyzdaCog(bot))
 
     synced = await bot.tree.sync(guild=discord.Object(id=settings.GUILD_ID))
     print(f"{len(synced)} cmds synced!")
