8ca02ad7ac2790e439ee034f04fdb76c3430090d
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2023-07-08 21:00:15 +0300

    switch to python, implement basic setup and faq command

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+import settings
+from cogs import faq
+
+bot = commands.Bot("!", intents=settings.intents)
+
+
+@bot.event
+async def on_ready():
+    await bot.add_cog(faq.Cog(bot))
+
+    synced = await bot.tree.sync(guild=discord.Object(id=settings.GUILD_ID))
+    print(f"{len(synced)} cmds synced!")
+
+
+bot.run(settings.DISCORD_BOT_TOKEN)
---
+++ settings.py
@@ -0,0 +1,9 @@
+import os
+
+import discord
+
+intents = discord.Intents.default()
+intents.members = True
+
+DISCORD_BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
+GUILD_ID = os.environ["GUILD_ID"]
