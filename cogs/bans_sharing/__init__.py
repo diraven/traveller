b226b9a6eef481d007c71c921c3645b14956d8ce
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2023-08-18 11:12:29 +0300

    feat: closes #82 - conditionally interactive ban message

--- cogs/bans_sharing/__init__.py
+++ cogs/bans_sharing/__init__.py
@@ -2,12 +2,13 @@ import logging
 import typing as t
 
 import discord
-import sentry_sdk
 import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 from discord.ext import commands
 
 import models
+
+from . import audit_log_ban_processor
 
 logger = logging.getLogger("mod")
 
@@ -25,99 +26,7 @@ async def setup(bot: commands.Bot) -> None:
 
         @commands.Cog.listener()
         async def on_audit_log_entry_create(self, entry: discord.AuditLogEntry) -> None:
-            if entry.action != discord.AuditLogAction.ban:
-                return
-            if not entry.target:
-                return
-
-            actor = t.cast(discord.Member, entry.user)
-            target = await bot.fetch_user(int(entry.target.id))
-            guild = entry.guild
-
-            if actor is None or target is None:
-                raise RuntimeError(
-                    f"Audit log entry: {entry.id} - missing actor or target."
-                )
-
-            logger.info("Ban event: %s - received.", target.id)
-            with sa_orm.Session(models.engine) as session, session.begin():
-                # Before doing anything else,
-                # make sure we did not see this ban already.
-                try:
-                    session.execute(
-                        sa.select(models.KnownBan).filter(
-                            models.KnownBan.id == target.id
-                        )
-                    ).one()
-                    logger.info("Ban event: %s - already seen, skipped.", target.id)
-                    return
-                except sa_orm.exc.NoResultFound:  # type: ignore
-                    pass
-
-                # Get channels we'll be sending notifications into.
-                # Getting all that are set except for originating server.
-                log_channels_ids = session.execute(
-                    sa.select(models.Guild.bans_sharing_channel_id).filter(
-                        models.Guild.bans_sharing_channel_id.is_not(None),
-                        models.Guild.id != guild.id,
-                    )
-                )
-                # And turn them into discord objects.
-                log_channels = [
-                    t.cast(discord.TextChannel, bot.get_channel(channel_id))
-                    for (channel_id,) in log_channels_ids
-                ]
-
-                # Create embed and populate it with data.
-                embed = discord.Embed(
-                    title=f"Бан на сервері {guild.name}",
-                )
-                embed.add_field(name="Сервер", value=f"{guild.name} ({guild.id})")
-                embed.add_field(
-                    name="Модератор",
-                    value=f"{actor.display_name} '{actor.name}' ({actor.id})",
-                )
-                embed.add_field(
-                    name="Забанений",
-                    value=(
-                        f"{target.mention} '{target.display_name}' '{target.name}' "
-                        f"({target.id})"
-                    ),
-                )
-                embed.add_field(name="Причина бану", value=entry.reason)
-                if target.avatar:
-                    embed.set_thumbnail(url=target.avatar.url)
-                embed.set_footer(
-                    text="Для створення такого самого бану на "
-                    "цьому сервері - скопіюйте та "
-                    "відправте текстову команду нижче."
-                )
-
-                # For each logging channel:
-                for log_channel in log_channels:
-                    try:
-                        msg = await log_channel.send(
-                            embed=embed,
-                        )
-                        reason = f" reason:{entry.reason}"
-                        # We have to leave "delete_messages" parameter below empty since
-                        # it's value depends on the language of the discord interface
-                        # and will give errors on language mismatch.
-                        # Post the message.
-                        await msg.reply(
-                            f"/ban user:{target.id} "
-                            f"delete_messages:{reason if entry.reason else ''}",
-                            suppress_embeds=True,
-                        )
-                    except discord.errors.Forbidden as exc:
-                        sentry_sdk.capture_exception(exc)
-
-                # Mark ban as seen.
-                logger.info(
-                    "Ban event: %s - is new, marking as seen.",
-                    target.id,
-                )
-                session.add(models.KnownBan(id=target.id, reason=entry.reason))
+            await audit_log_ban_processor.process(bot, entry)
 
         @root_command.command(description="Налаштувати канал сповіщень")  # type: ignore
         @discord.app_commands.guild_only()
@@ -250,5 +159,41 @@ async def setup(bot: commands.Bot) -> None:
             await interaction.response.send_message(
                 embed=embed,
             )
+
+        @root_command.command(  # type: ignore
+            description="Обробити останній бан користувача",
+        )
+        @discord.app_commands.guild_only()
+        @discord.app_commands.checks.has_permissions(administrator=True)
+        async def debug(
+            self,
+            interaction: discord.Interaction[commands.Bot],
+            moderator: discord.Member,
+        ) -> None:
+            if not interaction.guild:
+                return
+            guild = interaction.guild
+
+            # Try to find ban entry.
+            ban_entry: t.Optional[discord.AuditLogEntry] = None
+            async for entry in guild.audit_logs():
+                if (
+                    entry.user
+                    and entry.user.id == moderator.id
+                    and entry.action == discord.AuditLogAction.ban
+                ):
+                    ban_entry = entry
+
+            # Process entry or post response.
+            if ban_entry:
+                await audit_log_ban_processor.process(bot, ban_entry, debug=True)
+            else:
+                await interaction.response.send_message(
+                    embed=discord.Embed(
+                        title="Підходящий запис в лозі не знайдено",
+                        description="",
+                        color=discord.Color.red(),
+                    ),
+                )
 
     await bot.add_cog(BansSharingCog())
