f22ddcac85d637b0505027e379f6a6a3be18bb35
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2024-07-13 14:26:20 +0300

    feat: finish trusted moderators implementation

--- models.py
+++ models.py
@@ -7,14 +7,23 @@ import sqlalchemy as sa
 import sqlalchemy.orm as sa_orm
 from discord.ext import commands
 
-
 DB_URL = f'postgresql+psycopg://postgres:{os.environ["POSTGRES_PASSWORD"]}@{os.environ["PGHOST"]}:5432/postgres'
 engine = sa.create_engine(DB_URL)
 Session = sa_orm.sessionmaker(engine)
 
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = sa.MetaData(naming_convention=convention)
+
 
 class Base(sa_orm.DeclarativeBase):
-    pass
+    metadata = metadata
 
 
 class Guild(Base):
@@ -43,6 +52,7 @@ class Guild(Base):
 
 class BansSharingTrustedModerator(Base):
     __tablename__ = "bans_sharing_trusted_moderators"
+    __table_args__ = (sa.UniqueConstraint("guild_id", "user_id"),)
     id_: sa_orm.Mapped[int] = sa_orm.mapped_column(sa.BigInteger, primary_key=True)
     created_at: sa_orm.Mapped[datetime.datetime] = sa_orm.mapped_column(
         sa.types.DateTime(timezone=True),
@@ -51,6 +61,9 @@ class BansSharingTrustedModerator(Base):
     created_by: sa_orm.Mapped[int] = sa_orm.mapped_column(sa.BigInteger)
     guild_id: sa_orm.Mapped[int] = sa_orm.mapped_column(sa.BigInteger)
     user_id: sa_orm.Mapped[int] = sa_orm.mapped_column(sa.BigInteger)
+    user_global_name: sa_orm.Mapped[str] = sa_orm.mapped_column(
+        sa.String(32), unique=True
+    )
 
     def __repr__(self) -> str:
         return f"BansSharingTrustedModerator(created_by={self.created_by}, guild_id={self.guild_id}, user_id={self.user_id})"
